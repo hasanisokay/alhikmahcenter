6a26577b627de00e34c3a824f562d5637ce9895a
hasanisokay <hasanisokay@gmail.com>
2026-01-03 16:06:57 +0600

    changed meta image

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 import getThemeCookie from "@/utils/getThemeCookie.mjs";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
-import alhikmah from "@/../public/og-image.jpg";
+import alhikmah from "@/../public/images/og-image.jpg";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -70,7 +70,6 @@ export async function generateMetadata() {
       description,
       images: [
         {
-          // url: `${siteUrl}/og-image.jpg`,
           url:metaImage,
           width: 1200,
           height: 630,
